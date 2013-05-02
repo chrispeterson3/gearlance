28be9d9ca5788d68b458a86c3e88e456cf5bf512
Chris Peterson <peterson.m.chris@gmail.com>
2013-05-01 22:36:13 -0500

    Created Categories and Products Migrations

--- db/migrate/20130502032753_create_categories.rb
+++ db/migrate/20130502032753_create_categories.rb
@@ -1,5 +1,9 @@
 class CreateCategories < ActiveRecord::Migration
   def up
+    create_table :categories do |table|
+      table.string :name
+      table.string :slug
+    end
   end
 
   def down
--- db/migrate/20130502032804_create_products.rb
+++ db/migrate/20130502032804_create_products.rb
@@ -1,5 +1,14 @@
 class CreateProducts < ActiveRecord::Migration
   def up
+    create_table :products do |table|
+      table.integer :category_id
+      table.string :name
+      table.string :photo_url
+      table.text :description
+      table.integer :daily_price
+      table.integer :weekly_price
+      table.integer :monthly_price
+    end
   end
 
   def down
