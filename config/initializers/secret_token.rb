215515ea894d1f7ce90690e0f971568da18a4f14
Chris Peterson <peterson.m.chris@gmail.com>
2013-05-01 21:11:46 -0500

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gearlance::Application.config.secret_token = '542f584112f1a654b96fc07299c5c4337b400460640caefc9ff42ef7fad36b6b9702509d850ccea0a65e7a62b6eb35b72a3bbd7e42069fdfb3ea46be980732ca'
