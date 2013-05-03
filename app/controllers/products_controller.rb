ac738a72b827511f457398a693142d4ad3528124
Chris Peterson <peterson.m.chris@gmail.com>
2013-05-03 07:25:58 -0500

    Finished Post of a new Product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,19 @@ class ProductsController < ApplicationController
   end
 
   def new
-    
+
+  end
+
+  def create
+    p = Product.new
+    p.category_id = params[:category_id]
+    p.name = params[:name]
+    p.photo_url = params[:photo_url]
+    p.description = params[:description]
+    p.daily_price = params[:daily_price]
+    p.weekly_price = params[:weekly_price]
+    p.monthly_price = params[:monthly_price]
+    p.save
+    redirect_to products_url
   end
 end
