cb38b846c72c0c8b9e7ba01a53b22100fa089a41
Chris Peterson <peterson.m.chris@gmail.com>
2013-05-04 20:41:22 -0500

    Update to seeds file. Added relationship between Products and Categories.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 categories = [
-  { name: "Cameras", slug: "cameras" },
-  { name: "Lenses", slug: "lenses" }
+  { name: "Camera", slug: "cameras" },
+  { name: "Lens", slug: "lenses" }
 ]
 
 products = [
@@ -16,9 +16,9 @@ products = [
   { category_id: 1, name: "RCA Camcorder", photo_url: "http://th03.deviantart.net/fs71/PRE/f/2012/365/8/e/1986_rca_pro_wonder_vhs_camcorder_by_tikikah_the_phoenix-d5pvh9r.jpg", description: "Super cool description of product goes here.", daily_price: 10, weekly_price: 40, monthly_price: 100 },
   { category_id: 1, name: "EOS Rebel T2i", photo_url: "http://www.2dayblog.com/images/2010/february/t2i_1.jpg", description: "Super cool description of product goes here.", daily_price: 10, weekly_price: 40, monthly_price: 100 },
   { category_id: 1, name: "Lomokino Super 35mm", photo_url: "http://www.ubergizmo.com/wp-content/uploads/2011/11/17-LomoKino-Super-35.jpg", description: "Super cool description of product goes here.", daily_price: 10, weekly_price: 40, monthly_price: 100 },
-  { category_id: 2, name: "", photo_url: "http://ec1.images-amazon.com/images/I/41FER7HPR7L.jpg", description: "Super cool description of product goes here.", daily_price: 10, weekly_price: 40, monthly_price: 100 },
-  { category_id: 2, name: "", photo_url: "http://images04.olx.in/ui/13/04/88/1360851377_482469688_1-Pictures-of--Canon-DSLR-Lens-18-55mm.jpg", description: "Super cool description of product goes here.", daily_price: 10, weekly_price: 40, monthly_price: 100 },
-  { category_id: 2, name: "", photo_url: "http://2.bp.blogspot.com/-MwIlpDCqbX0/TmIjim9nYjI/AAAAAAAAACo/PDIpIiFqzEM/s1600/camera-lens-icon.jpg", description: "Super cool description of product goes here.", daily_price: 10, weekly_price: 40, monthly_price: 100 }
+  { category_id: 2, name: "Canon 85mm Lens", photo_url: "http://ec1.images-amazon.com/images/I/41FER7HPR7L.jpg", description: "Super cool description of product goes here.", daily_price: 10, weekly_price: 40, monthly_price: 100 },
+  { category_id: 2, name: "Canon 35mm Lens", photo_url: "http://images04.olx.in/ui/13/04/88/1360851377_482469688_1-Pictures-of--Canon-DSLR-Lens-18-55mm.jpg", description: "Super cool description of product goes here.", daily_price: 10, weekly_price: 40, monthly_price: 100 },
+  { category_id: 2, name: "Fake Lens", photo_url: "http://2.bp.blogspot.com/-MwIlpDCqbX0/TmIjim9nYjI/AAAAAAAAACo/PDIpIiFqzEM/s1600/camera-lens-icon.jpg", description: "Super cool description of product goes here.", daily_price: 10, weekly_price: 40, monthly_price: 100 }
 ]
 
 categories.each do |category|
