8e234153e1d6e98b7039f02756425e3bcc13950d
Chris Peterson <peterson.m.chris@gmail.com>
2013-05-14 22:45:07 -0500

    Adding basic navigation to get around. Saved user to DB when signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
   def index
+    @user = User.all
+  end
 
+  def new
+
+  end
+
+  def create
+    @user = User.new
+    @user.email = params[:email]
+    @user.first_name = params[:first_name]
+    @user.last_name = params[:last_name]
+    @user.save
+
+    redirect_to users_url
   end
 end
