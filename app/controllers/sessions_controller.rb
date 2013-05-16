907471435c60c8efed6633112d08ca3e063d7477
Chris Peterson <peterson.m.chris@gmail.com>
2013-05-15 21:27:54 -0500

    Added user sign in. Need to do Authnetication. Need to show that you are logged in and show log out button

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,20 @@
 class SessionsController < ApplicationController
   def new
+  end
 
+  def create
+    @user = User.find_by_email(params[:email])
+
+    if @user.present? && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to users_url, notice: "Successfully logged in!"
+    else
+      redirect_to new_session_url, notice: "Sign In unsuccessfull..why?"
+    end 
+  end
+
+  def delete
+    reset_session
+    redirect_to new_session_url, notice: "logged out"
   end
 end
