1cc0e7d6e15fa3333b112c511e49ceba6ad2ff1d
Chris Peterson <peterson.m.chris@gmail.com>
2013-05-15 20:33:07 -0500

    Added regular expression for email address validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
 
   validates :email, :first_name, :last_name, :password, presence: true
   validates :email, uniqueness: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, message: " looks like it's invalid"
 end
