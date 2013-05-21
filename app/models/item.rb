b94917d5e9fda368e01f43a6141b1760c737cf4f
Hard2Want <hard2want@hotmail.com>
2013-05-21 09:27:01 -0500

    remove merge conflict

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,6 @@ class Item < ActiveRecord::Base
   attr_accessible :category_id, :user_id, :name, :photo_url, :description, :daily_price, :weekly_price, :monthly_price, :category_slug
 
   belongs_to :category
-<<<<<<< HEAD
-
-end
-=======
   belongs_to :user
 
 end
->>>>>>> 01c071d6e97b419a41f3f0756e55f5732218b8bd
