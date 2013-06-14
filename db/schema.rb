390abb51a7bcd5038001c4852d079d5da24c7890
Chris Peterson <peterson.m.chris@gmail.com>
2013-06-14 14:39:41 -0500

    Updated Seeds. Hopefully will be good for prod

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(:version => 20130613135859) do
-=======
-ActiveRecord::Schema.define(:version => 20130613000324) do
->>>>>>> ea8719f1a72aabf94cd5a5c8b430a588dbcefb6d
 
   create_table "categories", :force => true do |t|
     t.string "name"
